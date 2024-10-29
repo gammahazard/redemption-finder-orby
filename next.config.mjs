d5d5f4d9da1a0264bf423a669eed31593dfe1d7d
gammahazard <dhaliswag777@gmail.com>
2024-10-29 16:22:55 -0400

    fix deploy by forcing npm install

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
-export default {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     env: {
       CRONOSCAN_API_KEY: process.env.CRONOSCAN_API_KEY
+    },
+    webpack: (config) => {
+      config.resolve.fallback = { fs: false, net: false, tls: false };
+      return config;
     }
-  }
+  };
   
+  export default nextConfig;
